317055be608c9c571b23f58f1c2c44087627b4cf
Hunter Lacefield <lace1010@gmail.com>
2021-03-10 10:56:48 +0800

    connect to mongoDB and save issue to issue_tracker database

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,73 @@
-'use strict';
+"use strict";
+const mongoose = require("mongoose"); // Need to require mongoose
 
-module.exports = function (app) {
+// Do everything inside this function so it is imported to server.js properly
+module.exports = (app) => {
+  let uri = process.env.ISSUE_TRACKER_MONGO_URI;
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  const Schema = mongoose.Schema;
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      let project = req.params.project;
-      
+  // Use the next four lines to see if you are conneted to mongoose correctly
+  var db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", () => {
+    console.log("Connection Successful!");
+  });
+
+  // Set a issueSchema
+  const issueSchema = new Schema({
+    issue_title: { type: String, required: true },
+    issue_text: { type: String, required: true },
+    created_by: { type: String, required: true },
+    assigned_to: String,
+    status_text: String,
+    created_on: { type: Date, required: true },
+    updated_on: { type: Date, required: true },
+    open: { type: Boolean, required: true },
+    project_name: String,
+  });
+
+  // Set a model for issue using issueSchema
+  const Issue = mongoose.model("Issue", issueSchema);
+
+  app
+    .route("/api/issues/:project")
+
+    .get((req, res) => {
+      let projectName = req.params.project;
     })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
+
+    .post((req, res) => {
+      let projectName = req.params.project;
+      let projectBody = req.body;
+      console.log(projectBody);
+      // Create new issue model to be stored in database
+      let newIssue = new Issue({
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || "",
+        status_text: req.body.status_text || "",
+        created_on: new Date().toUTCString(),
+        updated_on: new Date().toUTCString(),
+        open: true,
+        project_name: projectName,
+      });
+
+      // Save new issue to database
+      newIssue.save((error, savedIssue) => {
+        if (error) return console.log(error);
+        if (!error && savedIssue) {
+          console.log(savedIssue);
+        }
+      });
     })
-    
-    .put(function (req, res){
-      let project = req.params.project;
-      
+
+    .put((req, res) => {
+      let projectName = req.params.project;
     })
-    
-    .delete(function (req, res){
-      let project = req.params.project;
-      
+
+    .delete((req, res) => {
+      let projectName = req.params.project;
     });
-    
 };
